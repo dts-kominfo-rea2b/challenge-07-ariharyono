7287d2d3630b899a9805f1088019fd7f1a6229ad
Ari Haryono <33080550+ariharyono@users.noreply.github.com>
2022-06-10 21:56:24 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (surveyEmosi) => {
+  let sum = 0;
+  return new Promise((resolve, reject) => {
+    if (surveyEmosi === null || surveyEmosi === "") {
+      reject("SurveyEmosi tidak ada!");
+    } else {
+      promiseTheaterIXX()
+        .then((dataSurvey) => {
+          for (const data of dataSurvey) {if(surveyEmosi === data.hasil) {sum += 1;}
+          }
+        })
+        .then(
+          promiseTheaterVGC()
+          .then((dataSurvey) => {
+            dataSurvey.forEach((data) => {if(surveyEmosi === data.hasil) {sum += 1;}
+            });
+            resolve(sum);
+          })
+        );
+    }
+  });
+};
 
 module.exports = {
   promiseOutput,
